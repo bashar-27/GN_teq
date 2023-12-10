cbe02022287a931a9c7297f00ea0ede32cd33c25
bashar-27 <bashar0nidal@gmail.com>
2023-12-10 20:42:44 +0300

    Back

---
+++ Back/GnTeq/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿
+using GnTeq.Models;
+using GnTeq.Models.DTO;
+using GnTeq.Models.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
+
+using System;
+using System.Threading.Tasks;
+
+namespace GnTeq.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdmin _user;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AdminController(IAdmin user, SignInManager<AppUser> signInManager)
+        {
+            _user = user;
+            _signInManager = signInManager;
+        }
+
+
+        /// <summary>
+        /// Authenticate a user and perform login.
+        /// </summary>
+        /// <param name="loginDto">Login credentials.</param>
+        [HttpPost("Login")]
+        public async Task<ActionResult<AdminDTO>> Login(Login loginDto)
+        {
+            var user = await _user.Authenticate(loginDto.Username, loginDto.Password);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+            return user;
+        }
+
+        /// <summary>
+        /// Get the profile information of the currently authenticated user.
+        /// </summary>
+        [HttpGet("Logout")]
+        public async Task<ActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok("Logged Out");
+        }
+
+
+    }
+}
---
+++ Back/GnTeq/Controllers/EmployeeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GnTeq.Data;
+using GnTeq.Models;
+using GnTeq.Models.Interface;
+using Microsoft.AspNetCore.Authorization;
+using GnTeq.Models.Services;
+
+[Route("api/[controller]")]
+[ApiController]
+// [Authorize(Roles = "Administrator")]
+public class EmployeeController : ControllerBase
+{
+    private readonly IEmployee _employee;
+
+    // Constructor injection of IEmployee service
+    public EmployeeController(IEmployee employee)
+    {
+        _employee = employee;
+    }
+
+    // GET: api/Employee/GetEmployees
+    [HttpGet]
+    [Route("GetEmployees")]
+    //[Authorize(Roles = "Administrator")] // Requires Administrator role for access
+    public async Task<ActionResult<IEnumerable<Employee>>> GetUsers()
+    {
+        return await _employee.GetAllEmployee();
+    }
+
+    // GET: api/Employee/GetActiveEmployees
+    [HttpGet]
+    [Route("GetActiveEmployees")]
+    public async Task<ActionResult<IEnumerable<Employee>>> GetActiveUser()
+    {
+        return await _employee.GetActiveEmployees();
+    }
+
+    // GET: api/Employee/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Employee>> GetUser(int id)
+    {
+        return await _employee.GetEmployeeById(id);
+    }
+
+    // PUT: api/Employee/{id}
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutUser(int id, Employee user)
+    {
+        if (id != user.Id)
+        {
+            return BadRequest();
+        }
+
+        return Ok(await _employee.UpdateEmployee(id, user));
+    }
+
+    // POST: api/Employee
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Employee>> PostUser(Employee user)
+    {
+        return await _employee.CreateEmployee(user);
+    }
+
+    // DELETE: api/Employee/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        var employee = await _employee.GetEmployeeById(id);
+        if (id != employee.Id)
+        {
+            return BadRequest();
+        }
+        return Ok(await _employee.DeleteEmployee(id));
+    }
+}
